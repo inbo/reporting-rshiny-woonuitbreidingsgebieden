2651f0aab63e622e4caca1963ea0013a5b5fcc15
stijnvanhoey <stijnvanhoey@gmail.com>
2016-11-21 13:24:42 +0100

    create app creation function within package

---
+++ R/runWogApp.R
@@ -0,0 +1,14 @@
+#' Run the woonuitbreidingsgebieden application
+#' @return no return value
+#' @importFrom shiny runApp
+#' @export
+run_wog <- function() {
+    appDir <- system.file("shiny-examples", "wog",
+                          package = "woonuitbreidingsgebieden")
+    if (appDir == "") {
+        stop("Could not find example directory.
+             Try re-installing `mypackage`.", call. = FALSE)
+    }
+
+    runApp(appDir, display.mode = "normal")
+}

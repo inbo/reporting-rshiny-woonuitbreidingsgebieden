28eca55f30819f381629213446b79e582181cd5f
stijnvanhoey <stijnvanhoey@gmail.com>
2016-11-21 11:34:06 +0100

    adapt to package file locations

---
+++ R/create_radar.R
@@ -0,0 +1,47 @@
+#
+# Woonuitbreidingsgebieden
+#
+# S. Van Hoey
+# INBO
+#
+
+library(tidyr)
+library(radarchart)
+
+#' Plot an interactive radar/spider-chart of the ESD data towards a given
+#' reference
+#'
+#'@param ESD_data tidy version of the ESD data representation
+#'@param char choose an option from: wug, gemeente, vlaanderen,
+#' wug_gemeente, wug_provincie, wug_vlaanderen
+#'
+create_radar <- function(ESD_data, reference, threshold = 0.5){
+
+    current_sel <- ESD_data %>%
+        filter(type == "wug" | type == reference) %>%
+        spread(key = type, value = value)
+
+    # add the threshold
+    current_sel$threshold <- threshold
+    # sort the columns
+    current_sel <- current_sel[, c("ESD", "threshold", reference, "wug")]
+
+    # round the numbers for improved visualisation
+    current_sel[,-1] <- round(current_sel[,-1], 2)
+
+    colors <- matrix(c(128, 0, 0, ## versus200, 200, 200,
+                       49, 163, 84,
+                       217, 95, 14), nrow = 3, ncol = 3)
+
+    radar <- chartJSRadar(current_sel,
+                          responsive = TRUE,
+                          showToolTipLabel = TRUE,
+                          polyAlpha = 0.0,  # 0.3
+                          colMatrix = colors,
+                          addDots = FALSE,
+                          maxScale = 1.,
+                          scaleStartValue = 0.0,
+                          scaleStepWidth = 0.25
+                          )
+    return(radar)
+}
---
+++ R/server.R
@@ -0,0 +1,95 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(INBOtheme)
+library(radarchart)
+
+source("support.R")
+source("get_esd_data.R")
+source("get_landuse_data.R")
+source("create_radar.R")
+source("create_stacked_bar.R")
+source("create_loss_bar.R")
+
+# extract data
+xls_file <- "../data/Afwegingskader_Wug.xlsx"
+info_wug_ids <- readxl::read_excel(path = xls_file, sheet = "Info_Wug")
+ids_list <-  as.list(get_wug_ids(info_wug_ids))
+#province_list <- as.list(unique(info_wug$Provincie))
+
+shinyServer(function(input, output) {
+
+    output$wuglist <- renderUI({
+        selectizeInput(
+            inputId = "wug",
+            label = "WUG code:",
+            choices = ids_list,
+            selected = NULL,
+            options = list(
+                placeholder = 'Geef de WUG code in',
+                maxOptions = 1900,
+                onInitialize = I('function() { this.setValue(""); }'))
+        )
+    })
+
+    # create landuse figure percentages
+    lu_data <- reactive({
+                    if (is.null(input$wug)) {
+                        get_landuse_data_pt(xls_file, ids_list[1])}
+                    else if (input$wug == '') {
+                        get_landuse_data_pt(xls_file, ids_list[1])}
+                    else {get_landuse_data_pt(xls_file, input$wug)}
+                    })
+    output$barlu <- renderPlot({
+                        create_stacked_bar(lu_data())
+                        })
+    # create landuse figure percentage loss municipality
+    lu_data_ha <- reactive({
+        if (is.null(input$wug)) {
+            get_landuse_data_ha(xls_file, ids_list[1])}
+        else if (input$wug == '') {
+            get_landuse_data_ha(xls_file, ids_list[1])}
+        else {get_landuse_data_ha(xls_file, input$wug)}
+    })
+    output$barloss <- renderPlot({
+        create_loss_bar(lu_data_ha())
+    })
+
+    # create the radar chart
+    ESD_data <- reactive({
+                    if (is.null(input$wug) | input$wug == '') {
+                        get_esd_data(xls_file, ids_list[1])}
+                    else {get_esd_data(xls_file, input$wug)}
+                    })
+    output$radar <- renderChartJSRadar({
+        create_radar(ESD_data(), input$ref)
+        })
+
+    # Provide current WUG
+    output$wug_display <- renderText({
+        base_string <- "Ecosysteemdiensten WUG"
+        if (is.null(input$wug)) {
+            paste(base_string, ids_list[1])}
+        else if (input$wug == '') {
+            paste(base_string, ids_list[1])}
+        else {paste(base_string, input$wug)}
+        })
+    # Provide current municipality
+    output$gemeente_display <- renderText({
+        base_string <- "Procentueel verlies oppervlakte landgebruik"
+        if (is.null(input$wug)) {
+            base_string}
+        else if (input$wug == '') {
+            base_string}
+        else {paste(base_string, "in", names(lu_data_ha())[3])}
+    })
+
+
+})

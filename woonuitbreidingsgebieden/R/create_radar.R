63ffbb4261e2ab1f329d300097715226b183cbf2
stijnvanhoey <stijnvanhoey@gmail.com>
2017-01-05 17:11:04 +0100

    add non interactive radar chart with ggplot style

--- woonuitbreidingsgebieden/R/create_radar.R
+++ woonuitbreidingsgebieden/R/create_radar.R
@@ -5,8 +5,63 @@
 # INBO
 #
 
+#' Plot an nont-interactive ggplot style radar/spider-chart of the ESD data
+#' towards a given reference, using the custom ggradar function, derived from
+#' the ggradar package
+#'
+#' @param ESD_data tidy version of the ESD data representation
+#' @param reference char choose an option from: wug, gemeente, vlaanderen,
+#'     wug_gemeente, wug_provincie, wug_vlaanderen
+#' @param thresh provide the value on which a red threshold line should be
+#'     provided
+#' @param base.text.size int size of the textual graph elements
+#'
+#' @export
+#' @importFrom dplyr %>% filter_ mutate_ select_ mutate
+#' @importFrom tidyr spread_ gather_
+create_radar <- function(ESD_data, reference,
+                         thresh = 0.5,
+                         base.text.size = 16){
+
+    current_sel <- ESD_data %>%
+        spread_(key = "type", value = "value") %>%
+        mutate_("threshold" = thresh) %>%
+        select_("ESD", "wug", reference, "threshold") %>%
+        gather(key, value, -ESD) %>%
+        mutate(value = round(value, 2)) %>%
+        spread_(key = "ESD", value = "value") %>%
+        mutate(key = factor(key, levels = c(reference,
+                                            "wug",
+                                            "threshold"),
+                            ordered = TRUE))
+
+    colors <- c("#31a354", "#d95f0e", "#800000")
+
+    ggradar(plot.data = current_sel,
+            base.text.size = base.text.size,
+            grid.label.values = c(0, 0.5, 1),
+            plot.extent.x.sf = 1.2,
+            plot.extent.y.sf = 1.3,
+            axis.label.size = 5,
+            axis.label.offset = 1.1,
+            group.point.size = 2,
+            group.line.width = 0.5,
+            group.linecolors = colors,
+            axis.label.colour = inbo.steun.donkerroos,
+            grid.line.colour = 'grey',
+            grid.line.alpha = 0.5,
+            grid.label.size = 6,
+            grid.label.colour = inbo.grijs,
+            background.circle.colour = 'grey',
+            background.circle.transparency = 0.1,
+            plot.legend = TRUE,
+            legend.text.size = 12,
+            legend.text.colour = inbo.steun.donkerroos)
+}
+
+
 #' Plot an interactive radar/spider-chart of the ESD data towards a given
-#' reference
+#' reference, using the radarchart package
 #'
 #' @param ESD_data tidy version of the ESD data representation
 #' @param reference char choose an option from: wug, gemeente, vlaanderen,
@@ -18,14 +73,14 @@
 #' @importFrom dplyr %>% filter_
 #' @importFrom tidyr spread
 #' @importFrom radarchart chartJSRadar
-create_radar <- function(ESD_data, reference, threshold = 0.5){
+create_interactive_radar <- function(ESD_data, reference, thresh = 0.5){
 
     current_sel <- ESD_data %>%
         filter_(~type %in% c("wug", reference)) %>%
         spread(key = type, value = value)
 
     # add the threshold
-    current_sel$threshold <- threshold
+    current_sel$threshold <- thresh
     # sort the columns
     current_sel <- current_sel[, c("ESD", "threshold", reference, "wug")]
 

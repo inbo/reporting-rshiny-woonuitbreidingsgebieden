779d0a7f2db36b7b28d2c9de2979277ba9d53ddf
pauwel.rummens@cloudar.be <Ditr22djen00it>
2017-01-18 10:14:45 +0100

    create codedeploy base

---
+++ scripts/startshiny.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/ubuntu/woonuitbreidingsgebieden
+nohup java -jar shinyproxy-0.7.5.jar > logit 2>&1 &
---
+++ scripts/woonuitbreidingsgebieden.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Make sure shiny starts at boot
+mv /home/ubuntu/woonuitbreidingsgebieden/scripts/shinywoongebieden.service /etc/systemd/system
+systemctl enable shinywoongebieden.service
+# Make a tar.gz from the R-package from the code
+cd /home/ubuntu/woonuitbreidingsgebieden
+if [ -f woonuitbreidingsgebieden.tar.gz ]; then
+    rm woonuitbreidingsgebieden.tar.gz
+fi
+tar -zcvf woonuitbreidingsgebieden.tar.gz woonuitbreidingsgebieden
+# Build the docker image
+sudo docker build -t wug1 .

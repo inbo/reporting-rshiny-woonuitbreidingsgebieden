0d0586af05b0f8e4877e3c705896d999a9e06d50
pauwel.rummens@cloudar.be <Ditr22djen00it>
2017-01-18 18:53:42 +0100

    rights service file

--- scripts/woonuitbreidingsgebieden.sh
+++ scripts/woonuitbreidingsgebieden.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Make sure shiny starts at boot
 mv /home/ubuntu/woonuitbreidingsgebieden/scripts/woonuitbreidingsgebieden.service /etc/systemd/system
+chmod 777 /etc/systemd/system/woonuitbreidingsgebieden.service
 systemctl enable woonuitbreidingsgebieden.service
 # Make a tar.gz from the R-package from the code
 cd /home/ubuntu/woonuitbreidingsgebieden

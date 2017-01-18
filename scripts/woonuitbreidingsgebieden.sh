cef2000f9340f5ef451ee37e2cfefcd148a2dc87
pauwel.rummens@cloudar.be <Ditr22djen00it>
2017-01-18 19:30:07 +0100

    changes to init script

--- scripts/woonuitbreidingsgebieden.sh
+++ scripts/woonuitbreidingsgebieden.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Make sure shiny starts at boot
-mv /home/ubuntu/woonuitbreidingsgebieden/scripts/woonuitbreidingsgebieden.service /etc/systemd/system
-chmod 777 /etc/systemd/system/woonuitbreidingsgebieden.service
-chown root:root /etc/systemd/system/woonuitbreidingsgebieden.service
-systemctl enable woonuitbreidingsgebieden.service
+sudo mv /home/ubuntu/woonuitbreidingsgebieden/scripts/woonuitbreidingsgebieden /etc/init.d/
+sudo chmod 755 /etc/init.d/woonuitbreidingsgebieden
+sudo chown root:root /etc/systemd/system/woonuitbreidingsgebieden
+sudo update-rc.d woonuitbreidingsgebieden defaults
 # Make a tar.gz from the R-package from the code
 cd /home/ubuntu/woonuitbreidingsgebieden
 if [ -f woonuitbreidingsgebieden.tar.gz ]; then

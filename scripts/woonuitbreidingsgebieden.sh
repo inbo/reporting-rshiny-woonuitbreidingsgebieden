907ecdb5b9683a2c6468415a996f0ad631a42901
pauwel.rummens@cloudar.be <Ditr22djen00it>
2017-01-31 08:51:05 +0100

    moved over general app file to shiny-proxy repo

--- scripts/woonuitbreidingsgebieden.sh
+++ scripts/woonuitbreidingsgebieden.sh
@@ -2,7 +2,7 @@
 # Make sure shiny starts at boot
 sudo mv /home/ubuntu/woonuitbreidingsgebieden/scripts/woonuitbreidingsgebieden /etc/init.d/
 sudo chmod 755 /etc/init.d/woonuitbreidingsgebieden
-sudo chown root:root /etc/systemd/system/woonuitbreidingsgebieden
+sudo chown root:root /etc/init.d/woonuitbreidingsgebieden
 sudo update-rc.d woonuitbreidingsgebieden defaults
 # Make a tar.gz from the R-package from the code
 cd /home/ubuntu/woonuitbreidingsgebieden

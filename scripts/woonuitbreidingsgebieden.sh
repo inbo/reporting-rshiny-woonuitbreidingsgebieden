c5c9939cfc02f355716b3f62e8d65f9cdbf297fe
pauwel.rummens@cloudar.be <pauwel.rummens@cloudar.be>
2017-02-01 12:39:29 +0100

    moved back docket files

--- scripts/woonuitbreidingsgebieden.sh
+++ scripts/woonuitbreidingsgebieden.sh
@@ -11,5 +11,4 @@ if [ -f woonuitbreidingsgebieden.tar.gz ]; then
 fi
 tar -zcvf woonuitbreidingsgebieden.tar.gz woonuitbreidingsgebieden
 # Build the docker image
-cd /home/ubuntu
 sudo docker build -t wug1 .

b55997b7c1bef116fa4c237969c15c5d7c404be7
pauwel.rummens@cloudar.be <pauwel.rummens@cloudar.be>
2017-02-01 13:14:54 +0100

    changes in boot process of apps

--- scripts/startshiny.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/ubuntu
-nohup java -jar shinyproxy-0.7.5.jar > logit 2>&1 &
--- scripts/woonuitbreidingsgebieden.sh
+++ scripts/woonuitbreidingsgebieden.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Make sure shiny starts at boot
-sudo mv /home/ubuntu/woonuitbreidingsgebieden/scripts/woonuitbreidingsgebieden /etc/init.d/
-sudo chmod 755 /etc/init.d/woonuitbreidingsgebieden
-sudo chown root:root /etc/init.d/woonuitbreidingsgebieden
-sudo update-rc.d woonuitbreidingsgebieden defaults
 # Make a tar.gz from the R-package from the code
 cd /home/ubuntu/woonuitbreidingsgebieden
 if [ -f woonuitbreidingsgebieden.tar.gz ]; then
